6075c5d265cffbe67c3bbb02bbb282a8303ef771
Leed Almogbel <leedalmogbel@gmail.com>
2024-11-08 04:05:05 +0800

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='title'>
+        POKEMON SEARCH
+      </h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Bulbasaur</td>
+            <td>Grass, Poison</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default App
